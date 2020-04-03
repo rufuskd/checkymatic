579d55676e53c189a7f4541d9d7c0ed025e90a3c
Rufus <rufuskubedev@gmail.com>
2020-04-03 14:46:07 -0500

    Finally achieved full stack, database interaction through gunicorn works

--- gunicorncontainer/connectorApp/connectorApp.py
+++ gunicorncontainer/connectorApp/connectorApp.py
@@ -1,7 +1,20 @@
+import pymongo
+
 def app(environ, start_response):
+    client = pymongo.MongoClient('10.88.0.103', 27017)
+    db = client['testdb']
+    collection = db['testcollection']
+    testdoc = {
+        "Task": "Do a thing",
+        "Requirements": {
+            "Tomatoes": 3,
+            "Potatoes": 7
+        }
+    }
+    collection.insert_one(testdoc)
     data = b"Let's do this\n"
     start_response("200 OK", [
-        ("Content-Type", "test/plain"),
+        ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
--- gunicorncontainer/entrypoint.sh
+++ gunicorncontainer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env ash
 
 cd /usr/local/connectorApp
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 /usr/bin/gunicorn -b 0.0.0.0:8000 -w 4 connectorApp:app
