a16c2f72a5e329adbd6964df6df343fce482b27c
Rufus <rufuskubedev@gmail.com>
2020-04-06 12:53:55 -0500

    More suffering

--- gunicorncontainer/entrypoint.sh
+++ gunicorncontainer/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /usr/local/connectorApp
 pip3 install -r requirements.txt
-/usr/bin/gunicorn -b 0.0.0.0:8000 -w 4 connectorApp:app
+/usr/bin/gunicorn --log-file /var/log/gunerr.log --access-logfile /var/log/gunacc.log -b 0.0.0.0:8000 -w 4 connectorApp:app
