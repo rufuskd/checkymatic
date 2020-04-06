d51f1939d337acdc7262156c2d6a4b86ede7462b
Rufus <rufuskubedev@gmail.com>
2020-04-06 16:52:44 -0500

    Lots of work to try fix JSON thing

--- gunicorncontainer/connectorApp/connectorApp.py
+++ gunicorncontainer/connectorApp/connectorApp.py
@@ -4,24 +4,19 @@ def app(environ, start_response):
     client = pymongo.MongoClient('10.88.0.103', 27017)
     db = client['testdb']
     collection = db['testcollection']
-    testdoc = {
-        "Task": "Do a thing",
-        "Requirements": {
-            "Tomatoes": 3,
-            "Potatoes": 7
-        }
-    }
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except (ValueError):
         request_body_size = 0
     print(environ)
-    print("BENIS")
-    print(environ['wsgi.input'].read(request_body_size))
-    collection.insert_one(testdoc)
-    data = b"Let's do this\n"
+    if environ['REQUEST_METHOD'] == 'POST':
+        testdoc = environ['wsgi.input'].read(request_body_size)
+        #collection.insert_one(testdoc)
+        data = b"We ballin\n"
+    else:
+        data = b"Let's do this\n"
     start_response("200 OK", [
-        ("Content-Type", "text/plain"),
+        ("Content-Type", "application/json"),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
